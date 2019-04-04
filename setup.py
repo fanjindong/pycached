2ac133a8ca1cb90f3743b6ce0d9cab5e3bfeaf1f
fanjindong <jd.fan@neoclub.cn>
2019-04-04 10:08:15 +0800

    feat: init v0.0.1

--- setup.py
+++ setup.py
@@ -21,24 +21,22 @@ with open('README.rst', 'rt', encoding='utf8') as f:
 setup(
     name='pycached',
     version=version,
-    author='Manuel Miranda',
-    url='https://github.com/argaen/pycached',
-    author_email='manu.mirandad@gmail.com',
-    description='multi backend asyncio cache',
+    author='Fan Jindong',
+    url='https://github.com/fanjindong/pycached',
+    author_email='765912710@qq/com',
+    description='multi backend cache',
     long_description=readme,
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Framework :: AsyncIO',
     ],
     packages=find_packages(),
     install_requires=None,
     extras_require={
-        'redis:python_version<"3.7"': ['aioredis>=0.3.3'],
-        'redis:python_version>="3.7"': ['aioredis>=1.0.0'],
-        'memcached': ['aiomcache>=0.5.2'],
+        'redis:python_version<"3.7"': ['redis==2.10.6'],
+        'redis:python_version>="3.7"': ['redis==2.10.6'],
         'msgpack': ['msgpack'],
     }
 )
